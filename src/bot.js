7fce568978cbacf683fb7f7e95535b480d4f6c34
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-06-12 20:53:10 -0700

    Add method for bot to ban users from server

--- src/bot.js
+++ src/bot.js
@@ -35,7 +35,19 @@ client.on('message', async (message) => {
       } else {
         message.channel.send('That member was not found');
       }
-    } 
+    } else if (CMD_NAME === 'ban') {
+      if (!message.member.hasPermission('BAN_MEMBERS'))
+        return message.reply('You do not have permission to use that command');
+      if (args.length === 0) return message.reply('Please provide an ID');
+      try {
+        const user = await message.guild.members.ban(args[0]);
+        message.channel.send('User was banned successfully.');
+      } catch (err) {
+        message.channel.send(
+          'An error occured. Either I do not have permissions or the user was not found.'
+        );
+      }
+    }
   }
 });
 
