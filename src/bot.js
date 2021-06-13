77547a87fc6733ee4b444e63e213f93cf9e2135a
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-06-12 20:52:25 -0700

    Add method for bot to kick users from server

--- src/bot.js
+++ src/bot.js
@@ -12,5 +12,32 @@ client.on('ready', () => {
   console.log(`${client.user.tag} has logged in.`);
 });
 
+client.on('message', async (message) => {
+  if (message.author.bot) return;
+  if (message.content.startsWith(PREFIX)) {
+    const [CMD_NAME, ...args] = message.content
+      .trim()
+      .substring(PREFIX.length)
+      .split(/\s+/);
+    if (CMD_NAME === 'kick') {
+      if (!message.member.hasPermission('KICK_MEMBERS'))
+        return message.reply('You do not have permission to use that command');
+      if (args.length === 0) return message.reply('Please provide an ID');
+      const member = message.guild.members.cache.get(args[0]);
+
+      if (member) {
+        member
+          .kick()
+          .then((member) => message.channel.send(`${member} was kicked`))
+          .catch((err) =>
+            message.channel.send('I do not have permission to do that')
+          );
+      } else {
+        message.channel.send('That member was not found');
+      }
+    } 
+  }
+});
+
 // logs in the bot
 client.login(process.env.DISCORDJS_BOT_TOKEN);
