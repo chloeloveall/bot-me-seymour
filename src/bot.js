72a45fecec855e067e1c47d3f98168f2d8f2a299
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-07-03 16:13:38 -0700

    Add help command with embed

--- src/bot.js
+++ src/bot.js
@@ -216,18 +216,18 @@ client.on('message', async (message) => {
       };
       message.channel.send({ embed });
     } else if (CMD_NAME === 'help') {
-      message.channel.send(
-        ```
-        Bot Me, Seymour! available commands:
-
-        $kick + space + member ID: kicks selected member from the server (this command is only available to admins)
-        $ban + + space + member ID: bans selected member from the server (this command is only available to admins)
-        $pic + space + search term: searches for an image that matches the search term 
-        $plantpic: generates a random plant image
-        $fact: generates a random plant fact
-        $announce + space + message: announces the message
-        ```
-      );
+      const embedHelp = {
+        "commands": {
+          "title": "List of available Bot Me, Seymour! commands:",
+          "kick": "$kick + space + member ID: kicks selected member from the server (this command is only available to admins)",
+          "ban": "$ban + + space + member ID: bans selected member from the server (this command is only available to admins)",
+          "pic": "$pic + space + search term: searches for an image that matches the search term",
+          "plantpic": "$plantpic: generates a random plant image",
+          "fact": "$fact: generates a random plant fact",
+          "annouce": "$announce + space + message: announces the message"
+        }
+      };
+      message.channel.send({ embedHelp });
     } 
   }
 });
