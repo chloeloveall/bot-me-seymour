820f9d887ed1c253957f8279c952b9b669ecf8ea
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-07-03 13:49:21 -0700

    Add image command with test image #6

--- src/bot.js
+++ src/bot.js
@@ -18,53 +18,6 @@ const PREFIX = '$';
 // callback function parameter is how we handle the event
 client.on('ready', () => {
   console.log(`${client.user.tag} has logged in.`);
-  const embed = {
-    "title": "title ~~(did you know you can have markdown here too?)~~",
-    "description": "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
-    "url": "https://discordapp.com",
-    "color": 5811942,
-    "timestamp": "2021-07-03T20:45:43.950Z",
-    "footer": {
-      "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
-      "text": "footer text"
-    },
-    "thumbnail": {
-      "url": "https://cdn.discordapp.com/embed/avatars/0.png"
-    },
-    "image": {
-      "url": "https://cdn.discordapp.com/embed/avatars/0.png"
-    },
-    "author": {
-      "name": "author name",
-      "url": "https://discordapp.com",
-      "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
-    },
-    "fields": [
-      {
-        "name": "🤔",
-        "value": "some of these properties have certain limits..."
-      },
-      {
-        "name": "😱",
-        "value": "try exceeding some of them!"
-      },
-      {
-        "name": "🙄",
-        "value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
-      },
-      {
-        "name": "<:thonkang:219069250692841473>",
-        "value": "these last two",
-        "inline": true
-      },
-      {
-        "name": "<:thonkang:219069250692841473>",
-        "value": "are inline fields",
-        "inline": true
-      }
-    ]
-  };
-  channel.send("this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\nfunction foo(bar) {\n  console.log(bar);\n}\n\nfoo(1);```", { embed });
 });
 
 client.on('message', async (message) => {
@@ -177,6 +130,54 @@ client.on('message', async (message) => {
       const randomNum = Math.floor(Math.random() * facts.length);
       const fact = facts[randomNum];
       return message.channel.send(fact);
+    } else if (CMD_NAME === 'pic') {
+      const embed = {
+      "title": "title ~~(did you know you can have markdown here too?)~~",
+      "description": "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
+      "url": "https://discordapp.com",
+      "color": 5811942,
+      "timestamp": "2021-07-03T20:45:43.950Z",
+      "footer": {
+        "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+        "text": "footer text"
+      },
+      "thumbnail": {
+        "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+      },
+      "image": {
+        "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+      },
+      "author": {
+        "name": "author name",
+        "url": "https://discordapp.com",
+        "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+      },
+      "fields": [
+        {
+          "name": "🤔",
+          "value": "some of these properties have certain limits..."
+        },
+        {
+          "name": "😱",
+          "value": "try exceeding some of them!"
+        },
+        {
+          "name": "🙄",
+          "value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
+        },
+        {
+          "name": "<:thonkang:219069250692841473>",
+          "value": "these last two",
+          "inline": true
+        },
+        {
+          "name": "<:thonkang:219069250692841473>",
+          "value": "are inline fields",
+          "inline": true
+        }
+      ]
+    };
+    channel.send("this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\nfunction foo(bar) {\n  console.log(bar);\n}\n\nfoo(1);```", { embed });
     }
   }
 });
